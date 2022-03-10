d9b37d179ad1812009e25ff634cee636deed65c5
Mark Momot <markmomot03@gmail.com>
2022-03-10 17:31:57 -0500

    commit tree 15fa47bd8626185a09709c7f0fc7d2b37f11fc1f

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -3,4 +3,23 @@ class ApplicationController < Sinatra::Base
   
   # Add routes
   
+  get '/messages' do
+    Message.all.order(:created_at).to_json
+  end
+
+  post '/messages' do
+    Message.create(body: params[:body], username: params[:username]).to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+
+    message.update(body: params[:body])
+
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    Message.find(params[:id]).destroy.to_json
+  end
 end
